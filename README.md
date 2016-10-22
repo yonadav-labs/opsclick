80f7358cfa911bd1e73514c866bcc9cd051dbfad
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2016-10-22 17:44:34 +1100

    initial version

---
+++ README.md
@@ -0,0 +1,19 @@
+## Purpose
+
+Homepage for OpsClick.com
+
+It has CMS backend (Mezzanine) and django.
+It hires django-allauth for email verification, email based authentication, and possible social login.
+It is hosted on docker.
+
+## Run
+
+git clone the repo
+
+Run following commands:
+
+    sudo docker-compose down
+    sudo docker-compose up
+
+Open browser and connect 8000 port
+
---
+++ opsclick/local_settings.py
@@ -0,0 +1,52 @@
+# This file is exec'd from settings.py, so it has access to and can
+# modify all the variables in settings.py.
+
+# If this file is changed in development, the development server will
+# have to be manually restarted because changes will not be noticed
+# immediately.
+
+DEBUG = True
+
+# Make these unique, and don't share it with anybody.
+SECRET_KEY = "5r(hfb22vj)ss&+6+)gox^@2mi%%k6hek8l2wupe567^*_-x2a"
+NEVERCACHE_KEY = ")1a&*1=lca_&e=(w5ww5d@)!&#j5r5qs#0h334)dr^ic3f$$w%"
+
+DATABASES = {
+    "default": {
+        # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+        "ENGINE": "django.db.backends.sqlite3",
+        # DB name or path to database file if using sqlite3.
+        "NAME": "dev.db",
+        # Not used with sqlite3.
+        "USER": "",
+        # Not used with sqlite3.
+        "PASSWORD": "",
+        # Set to empty string for localhost. Not used with sqlite3.
+        "HOST": "",
+        # Set to empty string for default. Not used with sqlite3.
+        "PORT": "",
+    }
+}
+
+###################
+# DEPLOY SETTINGS #
+###################
+
+# Domains for public site
+# ALLOWED_HOSTS = [""]
+
+# These settings are used by the default fabfile.py provided.
+# Check fabfile.py for defaults.
+
+# FABRIC = {
+#     "DEPLOY_TOOL": "rsync",  # Deploy with "git", "hg", or "rsync"
+#     "SSH_USER": "",  # VPS SSH username
+#     "HOSTS": [""],  # The IP address of your VPS
+#     "DOMAINS": ALLOWED_HOSTS,  # Edit domains in ALLOWED_HOSTS
+#     "REQUIREMENTS_PATH": "requirements.txt",  # Project's pip requirements
+#     "LOCALE": "en_US.UTF-8",  # Should end with ".UTF-8"
+#     "DB_PASS": "",  # Live database password
+#     "ADMIN_PASS": "",  # Live admin user password
+#     "SECRET_KEY": SECRET_KEY,
+#     "NEVERCACHE_KEY": NEVERCACHE_KEY,
+# }
